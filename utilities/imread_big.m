a14a3bc948ad87de9b8814fe3a47da9f2a2d74da
Tom O'Connell <toconnel@caltech.edu>
2019-10-08 18:00:57 -0700

    Just fixed(?) line endings on imread_big and added a warning note about it's endianness limitations

--- utilities/imread_big.m
+++ utilities/imread_big.m
@@ -17,40 +17,45 @@
 % the size of each image.
 %
 
+% TODO TODO TODO if this is to work on a as many tiffs as possible,
+% needs to detect byte order as tifffile seems capable of
+% (right now, it seems to expect BIG endian TIFF byte order in uint16 case)
+% (doesn't matter for uint8 case, as only one byte)
+
 function [stack_out,Nframes] = imread_big(stack_name)
 
-%get data block size
+% get data block size
 info1 = imfinfo(stack_name);
 stripOffset = info1(1).StripOffsets;
 stripByteCounts = info1(1).StripByteCounts;
 
-%get image size
+% get image size
 sz_x=info1(1).Width;
 sz_y=info1(1).Height;
-if length(info1)<2
-    Nframes=floor(info1(1).FileSize/stripByteCounts);
+if length(info1) < 2
+    Nframes = floor(info1(1).FileSize / stripByteCounts);
 else
-    Nframes=length(info1);
+    Nframes = length(info1);
 end
 
-fID = fopen (stack_name, 'r');
+fID = fopen(stack_name, 'r');
 
-if info1(1).BitDepth==16
-    stack_out = zeros([sz_y sz_x Nframes],'uint16');
+if info1(1).BitDepth == 16
+    stack_out = zeros([sz_y sz_x Nframes], 'uint16');
 else
-    stack_out = zeros([sz_y sz_x Nframes],'uint8');
+    stack_out = zeros([sz_y sz_x Nframes], 'uint8');
 end
 
 start_point = stripOffset(1) + (0:1:(Nframes-1)).*stripByteCounts;
 
 for i = 1:Nframes
-    %fprintf ('loading image ... %d\n', i);
-    fseek (fID, start_point(i)+1, 'bof');
+    %fprintf('loading image ... %d\n', i);
+    fseek(fID, start_point(i)+1, 'bof');
     
-    if info1(1).BitDepth==16
-        A = fread (fID, [sz_x sz_y], 'uint16=>uint16');
+    if info1(1).BitDepth == 16
+        A = fread(fID, [sz_x sz_y], 'uint16=>uint16');
     else
-        A = fread (fID, [sz_x sz_y], 'uint8=>uint8');
+        A = fread(fID, [sz_x sz_y], 'uint8=>uint8');
     end
 
     % I found at least one case where the actual # frames was less than
@@ -59,8 +64,9 @@ for i = 1:Nframes
     % 2019-05-02/3/tif_stacks/fn_0001.tif was the failing example.
     if isempty(A)
         Nframes = i;
-        stack_out = stack_out(:,:,1:Nframes);
+        stack_out = stack_out(:, :, 1:Nframes);
         break;
     end
-    stack_out(:,:,i) = A';
+    stack_out(:, :, i) = A';
 end
+
